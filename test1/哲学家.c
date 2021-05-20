d1eca70863ec68d5a7dccdd06582cbd2b1fd1081
PENG <1337903705@qq.com>
2021-05-20 22:02:56 +0800

    两种方法实现哲学家

---
+++ test1/哲学家.c
@@ -0,0 +1,82 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include<stdio.h>
+sem_t bin_sem[5];
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+void my_error(const char *error_string, int line)
+{
+    fprintf(stderr, "line:%d",line);
+    perror(error_string);
+    
+}
+void thinking(int number)
+{
+    printf("thinker %d is thinking now\n",number);
+    sleep(2);
+}
+void eating(int number)
+{
+    printf("thinker %d is eating now\n",number);
+    sleep(3);
+}
+void *thinker(void *arg)
+{
+    int num=*(int*)arg;
+    pthread_mutex_unlock(&mutex);
+    while (1)
+    { 
+        thinking(num);
+        if (num%2==0)
+        {
+            sem_wait(&(bin_sem[num]));
+            sem_wait(&(bin_sem[(num+1)%5]));
+            eating(num);
+            sem_post(&(bin_sem[num]));
+            sem_post(&(bin_sem[(num+1)%5]));
+            printf("%d thinker is complete eating\n",num);
+        }
+        else{
+            sem_wait(&(bin_sem[(num+1)%5]));
+            sem_wait(&(bin_sem[num]));
+            eating(num);
+            sem_post(&(bin_sem[num]));
+            sem_post(&(bin_sem[(num+1)%5]));
+            printf("%d thinker is complete eating\n",num);
+        }
+        sleep(5);
+    }
+    exit(0);
+}
+int main()
+{
+    pthread_t pid[5];
+    int ret;
+    int i=0;
+    for (i=0; i<5; i++)
+    {
+        if ((ret=sem_init(&bin_sem[i],0,1))==-1)
+        {
+            my_error("sem_init",__LINE__);
+            exit(1);
+        }
+    }
+    for (i=0;i<5;i++)
+    {
+        pthread_mutex_lock(&mutex);
+        int index=i;
+        int *a=&index;
+        if ((ret=pthread_create(&(pid[i]),NULL,thinker,a))!=0)
+        {
+            my_error("pthread_cread",__LINE__);
+            exit(1);
+        }
+    }
+    for (i=0; i<5; i++)
+    {
+        pthread_join(pid[i],NULL);
+    }
+    
+}
